30438966ce938621f6e8c7427475b770ffb8b66d
Moringa School <lizdmukami@gmail.com>
2022-12-16 14:16:13 +0300

    Updated CSS and added the hero section

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() { 
+
+    const header = document.querySelector("header");
+
+    window.addEventListener ("scroll", function() {
+        header.classList.toggle ("sticky", window.scrollY > 0);
+    });
+
+    let menu = document.querySelector('#menu-icon');
+    let navlist = document.querySelector('.navlist');
+
+    menu.onclick = () => {
+        menu.classList.toggle('bx-x');
+        navlist.classList.toggle('open');
+    };
+
+    window.onscroll = () => {
+        menu.classList.remove('bx-x');
+        navlist.classList.remove('open');
+    };
+
     let products = document.querySelector('.products')
     async function fetchProducts(url){
         let data = await fetch(url);
@@ -10,12 +30,14 @@ document.addEventListener('DOMContentLoaded', function() {
         products.innerHTML +=`
         <div class="product">
             <img src="${response[i].image}" alt="" class="product-img">
-            <h2 class="product-title">${response[i].title}</h2>
-            <h4 class="product-category">${response[i].category}</h4>
-            <p class="product-description">${description.length > 100 ? description.substring(0, 130).concat('  ....more') : description}</p>
-            <div class="product-price-container">
-                <h3 class="product-price">$ ${response[i].price}</h3>
-                <a href="#!" data-productId= "${response[i].id}" class="add-to-cart">Add To Cart</a>
+            <div class="product-content">
+                <h2 class="product-title">${response[i].title}</h2>
+                <h4 class="product-category">${response[i].category}</h4>
+                
+                <div class="product-price-container">
+                    <h3 class="product-price">$ ${response[i].price}</h3>
+                    <a href="#!" data-productId= "${response[i].id}" class="add-to-cart">Add To Cart</a>
+                </div>
             </div>
         </div>`;
     
