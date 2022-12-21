2169787cd62c64441cb92a11b2a57998da9cd9a9
lizdmukami <lizdmukami@gmail.com>
2022-12-21 16:53:08 +0300

    Added an id to the buy button

--- script.js
+++ script.js
@@ -92,14 +92,14 @@ document.addEventListener('DOMContentLoaded', function() {
       let div = document.createElement('div');
       div.className = 'item justify-self-center';
 
-      let { image, title, category, price, id } = value;
+      let { image, title, category, price } = value;
       div.innerHTML = `
               <img src="${image}" class="bg-cover img mx-auto" alt="img1">
               <div class="text-center py-3 font-poppins">
                   <h1 class="text-lg title">${title}</h1>
                   <a href="#" class="block"><span class="text-sm text-red-400">${category}</span></a>
                   <span class="block py-3">$<span class="text-md">${price}</span></span>
-                  <button class="border-2 px-8 py-1 bg-yellow-400 border rounded-md" id="${id}">Buy Now</button>
+                  <button class="border-2 px-8 py-1 bg-yellow-400 border rounded-md">Buy Now</button>
               </div>
       `;
       appendIn.appendChild(div); 
